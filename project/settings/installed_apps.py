c8dc644a7f26199eef79b10a688a4258c27f7af6
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-04-10 11:19:40 -0300

    Refactor installed_apps and signals; remove unnecessary comments and improve instance retrieval logic

--- project/settings/installed_apps.py
+++ project/settings/installed_apps.py
@@ -1,11 +1,14 @@
 INSTALLED_APPS = [
+    # DJANGO
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # A gente tem apps também
+    # DJANGO REST APIs
+    'rest_framework',
+    # MY APPs
     'recipes',
     'authors',
     'tag',
