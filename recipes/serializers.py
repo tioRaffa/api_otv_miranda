d185158848e7aa7b15f74e7dcf3e46003ff1f605
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-04-11 10:14:14 -0300

    Enhance RecipeManager and RecipeSerializer: add related fields and improve preparation/serving formatting

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,16 +1,38 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from tag.models import Tag
 
 class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=244)
     description = serializers.CharField(max_length=165)
     slug = serializers.SlugField()
-    preparation_time = serializers.IntegerField()
-    preparation_time_unit = serializers.CharField(max_length=65)
-    servings = serializers.IntegerField()
-    servings_unit = serializers.CharField(max_length=65)
+    
+    preparation = serializers.SerializerMethodField()
+    def get_preparation(self, recipe):
+        return f'{recipe.preparation_time} - {recipe.preparation_time_unit}'
+    
+    serving = serializers.SerializerMethodField()
+    def get_serving(self, recipe):
+        return f'{recipe.servings} - {recipe.servings_unit}'
+    
     preparation_steps = serializers.CharField()
     preparation_steps_is_html = serializers.BooleanField(default=False)
+    
+    # category = serializers.PrimaryKeyRelatedField(
+    #     queryset=Category.objects.all()               ID da categoria
+    # )
+    category = serializers.StringRelatedField() # Nome da categoria
+    author = serializers.StringRelatedField()
+    
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True
+    )
+    
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
     is_published = serializers.BooleanField(default=False)
+    
+    
+    
